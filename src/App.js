53777e6d056681281e2e1f8f80ea2418b35217d0
Jeremy Myers <inernestmusic@gmail.com>
2021-10-26 13:47:38 -0400

    changed box-sizing to border-box for whole page to ensure pointer displays in the center of the mouse click, irrespecive of border thickness. refactored such that the park-selector-container has the width variable so that the buttons are the width of their respective textContent. made the park-selector ul a flex container that displays flex-start or flex-end depending on which side of the pointer display the selector is rendered. added functionality and algorithms to determine which side of the pointer display to show the selector so that the selector is contained within the image. also, clicking on a selector button now removes the display of the pointer and selector.

--- src/App.js
+++ src/App.js
@@ -11,25 +11,69 @@ function App() {
   const clickLocation = (e) => {
     const pointer = document.querySelector(`.pointer`);
     const imgCollage = document.querySelector(`#img-collage`);
-    console.log(imgCollage);
+    // console.log(imgCollage);
+    console.log(e.target.className);
     if (
       e.pageX >= imgCollage.offsetLeft &&
       e.pageX <= imgCollage.offsetLeft + imgCollage.width &&
       e.pageY >= imgCollage.offsetTop &&
-      e.pageY <= imgCollage.offsetTop + imgCollage.height
+      e.pageY <= imgCollage.offsetTop + imgCollage.height &&
+      e.target.className !== `choose-park-btn`
     ) {
+      // display properties for the pointer
       const pointerDiameterString = window
         .getComputedStyle(pointer)
         .getPropertyValue(`width`);
-      const pointerDiameterInteger = pointerDiameterString.substring(
+      const pointerDiameterInteger = +pointerDiameterString.substring(
         0,
         pointerDiameterString.length - 2
       );
       pointer.style.display = `block`;
       pointer.style.left = `${e.pageX - pointerDiameterInteger / 2}px`;
       pointer.style.top = `${e.pageY - pointerDiameterInteger / 2}px`;
+
+      //display properties for the selectors
+      const selectorContainer = document.querySelector(
+        `#park-selector-container`
+      );
+      // const selectorBtn = document.querySelector(`.choose-park-btn`);
+      const selectorContainerWidthString = window
+        .getComputedStyle(selectorContainer)
+        .getPropertyValue(`width`);
+      const selectorContainerWidthInteger =
+        +selectorContainerWidthString.substring(
+          0,
+          selectorContainerWidthString.length - 2
+        );
+      // const selectAllBtns = document.querySelectorAll(`.choose-park-btn`);
+      const parkSelector = document.querySelector(`#park-selector`);
+      if (
+        e.pageX +
+          10 +
+          selectorContainerWidthInteger +
+          pointerDiameterInteger / 2 <=
+        imgCollage.offsetLeft + imgCollage.width
+      ) {
+        selectorContainer.style.left = `${pointerDiameterInteger}px`;
+        selectorContainer.style.right = ``;
+        // selectAllBtns.forEach((btn) => {
+        //   btn.style.textAlign = `left`;
+        // });
+        parkSelector.style.alignItems = `flex-start`;
+      } else {
+        selectorContainer.style.left = ``;
+        selectorContainer.style.right = `${
+          selectorContainerWidthInteger + pointerDiameterInteger / 5
+        }px`;
+        // selectAllBtns.forEach((btn) => {
+        //   btn.style.textAlign = `right`;
+        // });
+        parkSelector.style.alignItems = `flex-end`;
+      }
     } else {
       pointer.style.display = `none`;
+      pointer.style.left = ``;
+      pointer.style.top = ``;
     }
   };
 
