dba0c60da7f4ad343e5de8c447dd508e90fd1260
Jeremy Myers <inernestmusic@gmail.com>
2021-10-26 11:57:51 -0400

    set the click eventListener to the window instead of the image in order to toggle the pointer display based on whether user clicked on or off the image. the algorithms to determine if user clicks on or off image is based on img properties in js and will extend to any changes in the photo collage dimensions.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 // import photoCollage from "./assets/national-parks-collage.jpg";
 import photoCollage2 from "./assets/national-parks-collage-2.jpg";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener(`click`, clickLocation);
+  }, []);
+
   const clickLocation = (e) => {
     const pointer = document.querySelector(`.pointer`);
-    const pointerDiameterString = window
-      .getComputedStyle(pointer)
-      .getPropertyValue(`width`);
-    const pointerDiameterInteger = pointerDiameterString.substring(
-      0,
-      pointerDiameterString.length - 2
-    );
-    pointer.style.display = `block`;
-    pointer.style.left = `${e.clientX - pointerDiameterInteger / 2}px`;
-    pointer.style.top = `${e.clientY - pointerDiameterInteger / 2}px`;
+    const imgCollage = document.querySelector(`#img-collage`);
+    console.log(imgCollage);
+    if (
+      e.pageX >= imgCollage.offsetLeft &&
+      e.pageX <= imgCollage.offsetLeft + imgCollage.width &&
+      e.pageY >= imgCollage.offsetTop &&
+      e.pageY <= imgCollage.offsetTop + imgCollage.height
+    ) {
+      const pointerDiameterString = window
+        .getComputedStyle(pointer)
+        .getPropertyValue(`width`);
+      const pointerDiameterInteger = pointerDiameterString.substring(
+        0,
+        pointerDiameterString.length - 2
+      );
+      pointer.style.display = `block`;
+      pointer.style.left = `${e.pageX - pointerDiameterInteger / 2}px`;
+      pointer.style.top = `${e.pageY - pointerDiameterInteger / 2}px`;
+    } else {
+      pointer.style.display = `none`;
+    }
   };
 
   return (
@@ -25,10 +41,31 @@ function App() {
         alt="A collage of imagery representing the different National Parks of the United States."
       ></img> */}
       <div className="image-container">
-        <div className="pointer"></div>
+        <div className="pointer">
+          <div id="park-selector-container">
+            <ul id="park-selector">
+              <li>
+                <button className="choose-park-btn" value="bigBend">
+                  Big Bend
+                </button>
+              </li>
+              <li>
+                <button className="choose-park-btn" value="smokies">
+                  Smoky Mountains
+                </button>
+              </li>
+              <li>
+                <button className="choose-park-btn" value="guadalupe">
+                  Guadalupe Mountains
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
         <img
+          id="img-collage"
           src={photoCollage2}
-          onClick={clickLocation}
+          // onClick={clickLocation}
           alt="A collage of imagery representing the different National Parks of the United States."
         ></img>
       </div>
@@ -37,3 +74,12 @@ function App() {
 }
 
 export default App;
+
+// {/* <label for="choose-park">Choose a pet:</label> */}
+
+// <select name="park" id="choose-park">
+//   {/* <option value="">--Please choose an option--</option> */}
+//   <option value="bigBend">Big Bend</option>
+//   <option value="smokyMtns">Smoky Mountains</option>
+//   <option value="guadalupeMtns">Guadalupe Mountains</option>
+// </select>
