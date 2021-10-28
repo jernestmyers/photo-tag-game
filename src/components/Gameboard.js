2bc25d3b921eafe3de4adce477cfc7637ce85c5b
Jeremy Myers <inernestmusic@gmail.com>
2021-10-28 11:23:49 -0400

    repurposed the divs in Gameboard that previously showed the target areas that were used to determine the relative and absolute location data that is now stored in firestore. the divs now have an SVG of a checkmark and the div is set to display when a target emblem is found. the div is dynamically set with absolute positioning and the clickLocation data.

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -126,70 +126,70 @@ function Gameboard(props) {
     }
     // END - display properties for the pointer
 
-    drawAnswerBoxes(imgCollage);
+    // drawAnswerBoxes(imgCollage);
   };
 
-  function drawAnswerBoxes(img) {
-    const bigBendBox = document.querySelector(`#big-bend-answer`);
-    bigBendBox.style.width = `${img.width * (75 / img.naturalWidth)}px`;
-    bigBendBox.style.height = `${img.height * (80 / img.naturalHeight)}px`;
-    bigBendBox.style.left = `${
-      img.offsetLeft + img.width * ((565 - 77) / img.naturalWidth)
-    }px`;
-    bigBendBox.style.top = `${
-      img.height * ((70 - 16) / img.naturalHeight) + img.offsetTop
-    }px`;
+  //   function drawAnswerBoxes(img) {
+  //     const bigBendBox = document.querySelector(`#big-bend-answer`);
+  //     bigBendBox.style.width = `${img.width * (75 / img.naturalWidth)}px`;
+  //     bigBendBox.style.height = `${img.height * (80 / img.naturalHeight)}px`;
+  //     bigBendBox.style.left = `${
+  //       img.offsetLeft + img.width * ((565 - 77) / img.naturalWidth)
+  //     }px`;
+  //     bigBendBox.style.top = `${
+  //       img.height * ((70 - 16) / img.naturalHeight) + img.offsetTop
+  //     }px`;
 
-    const glacierBox = document.querySelector(`#glacier-answer`);
-    glacierBox.style.width = `${img.width * (78 / img.naturalWidth)}px`;
-    glacierBox.style.height = `${img.height * (95 / img.naturalHeight)}px`;
-    glacierBox.style.left = `${
-      img.offsetLeft + img.width * ((665 - 77) / img.naturalWidth)
-    }px`;
-    glacierBox.style.top = `${
-      img.height * ((280 - 16) / img.naturalHeight) + img.offsetTop
-    }px`;
+  //     const glacierBox = document.querySelector(`#glacier-answer`);
+  //     glacierBox.style.width = `${img.width * (78 / img.naturalWidth)}px`;
+  //     glacierBox.style.height = `${img.height * (95 / img.naturalHeight)}px`;
+  //     glacierBox.style.left = `${
+  //       img.offsetLeft + img.width * ((665 - 77) / img.naturalWidth)
+  //     }px`;
+  //     glacierBox.style.top = `${
+  //       img.height * ((280 - 16) / img.naturalHeight) + img.offsetTop
+  //     }px`;
 
-    const guadBox = document.querySelector(`#guad-answer`);
-    guadBox.style.width = `${img.width * (70 / img.naturalWidth)}px`;
-    guadBox.style.height = `${img.height * (70 / img.naturalHeight)}px`;
-    guadBox.style.left = `${
-      img.offsetLeft + img.width * ((130 - 77) / img.naturalWidth)
-    }px`;
-    guadBox.style.top = `${
-      img.height * ((340 - 16) / img.naturalHeight) + img.offsetTop
-    }px`;
+  //     const guadBox = document.querySelector(`#guad-answer`);
+  //     guadBox.style.width = `${img.width * (70 / img.naturalWidth)}px`;
+  //     guadBox.style.height = `${img.height * (70 / img.naturalHeight)}px`;
+  //     guadBox.style.left = `${
+  //       img.offsetLeft + img.width * ((130 - 77) / img.naturalWidth)
+  //     }px`;
+  //     guadBox.style.top = `${
+  //       img.height * ((340 - 16) / img.naturalHeight) + img.offsetTop
+  //     }px`;
 
-    const joshuaBox = document.querySelector(`#joshua-answer`);
-    joshuaBox.style.width = `${img.width * (70 / img.naturalWidth)}px`;
-    joshuaBox.style.height = `${img.height * (70 / img.naturalHeight)}px`;
-    joshuaBox.style.left = `${
-      img.offsetLeft + img.width * ((387 - 77) / img.naturalWidth)
-    }px`;
-    joshuaBox.style.top = `${
-      img.height * ((700 - 16) / img.naturalHeight) + img.offsetTop
-    }px`;
+  //     const joshuaBox = document.querySelector(`#joshua-answer`);
+  //     joshuaBox.style.width = `${img.width * (70 / img.naturalWidth)}px`;
+  //     joshuaBox.style.height = `${img.height * (70 / img.naturalHeight)}px`;
+  //     joshuaBox.style.left = `${
+  //       img.offsetLeft + img.width * ((387 - 77) / img.naturalWidth)
+  //     }px`;
+  //     joshuaBox.style.top = `${
+  //       img.height * ((700 - 16) / img.naturalHeight) + img.offsetTop
+  //     }px`;
 
-    const rockiesBox = document.querySelector(`#rockies-answer`);
-    rockiesBox.style.width = `${img.width * (65 / img.naturalWidth)}px`;
-    rockiesBox.style.height = `${img.height * (63 / img.naturalHeight)}px`;
-    rockiesBox.style.left = `${
-      img.offsetLeft + img.width * ((590 - 77) / img.naturalWidth)
-    }px`;
-    rockiesBox.style.top = `${
-      img.height * ((880 - 16) / img.naturalHeight) + img.offsetTop
-    }px`;
+  //     const rockiesBox = document.querySelector(`#rockies-answer`);
+  //     rockiesBox.style.width = `${img.width * (65 / img.naturalWidth)}px`;
+  //     rockiesBox.style.height = `${img.height * (63 / img.naturalHeight)}px`;
+  //     rockiesBox.style.left = `${
+  //       img.offsetLeft + img.width * ((590 - 77) / img.naturalWidth)
+  //     }px`;
+  //     rockiesBox.style.top = `${
+  //       img.height * ((880 - 16) / img.naturalHeight) + img.offsetTop
+  //     }px`;
 
-    const saguaroBox = document.querySelector(`#saguaro-answer`);
-    saguaroBox.style.width = `${img.width * (84 / img.naturalWidth)}px`;
-    saguaroBox.style.height = `${img.height * (83 / img.naturalHeight)}px`;
-    saguaroBox.style.left = `${
-      img.offsetLeft + img.width * ((218 - 77) / img.naturalWidth)
-    }px`;
-    saguaroBox.style.top = `${
-      img.height * ((950 - 16) / img.naturalHeight) + img.offsetTop
-    }px`;
-  }
+  //     const saguaroBox = document.querySelector(`#saguaro-answer`);
+  //     saguaroBox.style.width = `${img.width * (84 / img.naturalWidth)}px`;
+  //     saguaroBox.style.height = `${img.height * (83 / img.naturalHeight)}px`;
+  //     saguaroBox.style.left = `${
+  //       img.offsetLeft + img.width * ((218 - 77) / img.naturalWidth)
+  //     }px`;
+  //     saguaroBox.style.top = `${
+  //       img.height * ((950 - 16) / img.naturalHeight) + img.offsetTop
+  //     }px`;
+  //   }
 
   const validateSelection = async (e) => {
     const targetClicked = e.target.value;
@@ -209,7 +209,23 @@ function Gameboard(props) {
           }
         })
       );
+
+      // BEGIN - turns on class to set opacity of emblem to 12.5% and displays a checkmark at the clickLocation
       document.querySelector(`#${targetClicked}`).classList.add(`target-found`);
+      const body = document.querySelector(`body`);
+      const getFontSize = window
+        .getComputedStyle(body)
+        .getPropertyValue(`font-size`);
+      document.querySelector(
+        `#${targetClicked}-checkbox`
+      ).style.display = `block`;
+      document.querySelector(`#${targetClicked}-checkbox`).style.left = `${
+        clickLocation.x - +getFontSize.substring(0, getFontSize.length - 2)
+      }px`;
+      document.querySelector(`#${targetClicked}-checkbox`).style.top = `${
+        clickLocation.y - +getFontSize.substring(0, getFontSize.length - 2)
+      }px`;
+      // END - turns on class to set opacity of emblem to 12.5% and displays a checkmark at the clickLocation
     } else {
       console.log(`${targetClicked} was NOT successfully clicked`);
     }
@@ -217,12 +233,90 @@ function Gameboard(props) {
 
   return (
     <div className="image-container">
-      <div id="big-bend-answer"></div>
-      <div id="glacier-answer"></div>
-      <div id="guad-answer"></div>
-      <div id="joshua-answer"></div>
-      <div id="rockies-answer"></div>
-      <div id="saguaro-answer"></div>
+      <div className="checkbox" id="bigBend-checkbox">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="far"
+          data-icon="check-circle"
+          className="svg-inline--fa fa-check-circle fa-w-16"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+        </svg>
+      </div>
+      <div className="checkbox" id="glacier-checkbox">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="far"
+          data-icon="check-circle"
+          className="svg-inline--fa fa-check-circle fa-w-16"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+        </svg>
+      </div>
+      <div className="checkbox" id="guadalupe-checkbox">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="far"
+          data-icon="check-circle"
+          className="svg-inline--fa fa-check-circle fa-w-16"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+        </svg>
+      </div>
+      <div className="checkbox" id="joshuaTree-checkbox">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="far"
+          data-icon="check-circle"
+          className="svg-inline--fa fa-check-circle fa-w-16"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+        </svg>
+      </div>
+      <div className="checkbox" id="rockies-checkbox">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="far"
+          data-icon="check-circle"
+          className="svg-inline--fa fa-check-circle fa-w-16"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+        </svg>
+      </div>
+      <div className="checkbox" id="saguaro-checkbox">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="far"
+          data-icon="check-circle"
+          className="svg-inline--fa fa-check-circle fa-w-16"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+        </svg>
+      </div>
       <div className="pointer">
         <TargetSelector
           validateSelection={validateSelection}
